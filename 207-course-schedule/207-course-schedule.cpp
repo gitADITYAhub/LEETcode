3fd3c54531876bc6e9d8d2126926ec235535151d
gitADITYAhub <99046540+gitADITYAhub@users.noreply.github.com>
2022-07-02 20:53:05 +0530

    Time: 42 ms (29.84%), Space: 15.8 MB (11.90%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.cpp
@@ -0,0 +1,78 @@
+class Solution {
+public:
+//     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+//         int n=prerequisites.size();
+//         int c=0;
+//         vector<vector<int>>graph;
+//         vector<int>indegree(n,0);
+//         queue<int>q;
+//          for(auto edge : prerequisites)
+//         {
+//             graph[edge.second].push_back(edge.first);
+//             ++indegree[edge.first];
+//         }
+//         // for(int i=0;i<n;i++){
+//         //     for(auto x:prerequisites[0[i]])
+//         //         indegree[x]++;
+//         // }
+//         for(int i=0;i<v;i++){
+//             if(indegree[i]==0)
+//                 q.push(i);
+//         }
+        
+//         while(!q.empty()){
+//             int u=q.front();
+//             q.pop();
+//             for(auto v:graph){
+//                 indegree[v]--;
+//                 if(indegree[v]==0)
+//                     q.push(v);
+//             }
+//             c++;
+//         }
+//         return c==numCourses;
+//     }
+     bool DFSRec(int s, vector<bool>&visited, vector<bool>&currVisited, vector<int>adj[]){
+        visited[s] = true;
+        currVisited[s] = true;
+        
+        vector<int>data = adj[s];
+        for(auto x: data){
+            if(!visited[x]){
+                if(DFSRec(x, visited, currVisited, adj)){
+                    return true;
+                }
+            }
+            else if(visited[x] && currVisited[x]){
+                return true;
+            }
+        }
+        
+        currVisited[s] = false;
+        return false;
+    }
+    
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        
+        int n = numCourses;
+        vector<bool>visited(n, false), currVisited(n, false);
+        
+        vector<int>adj[n];
+        for(auto x: prerequisites){
+            vector<int>data = x;
+            int a = data[0];
+            int b = data[1];
+            adj[a].push_back(b);
+        }
+        
+        for(int i=0; i<n; i++){
+            if(!visited[i]){
+                if(DFSRec(i, visited, currVisited, adj)){
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+};
