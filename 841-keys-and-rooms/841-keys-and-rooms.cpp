975b16c5b8f81a8dd6d31b13daa46e22608c10a7
gitADITYAhub <99046540+gitADITYAhub@users.noreply.github.com>
2022-07-10 17:15:24 +0530

    Time: 14 ms (59.28%), Space: 10.3 MB (78.92%) - LeetHub

--- 841-keys-and-rooms/841-keys-and-rooms.cpp
+++ 841-keys-and-rooms/841-keys-and-rooms.cpp
@@ -1,32 +1,65 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& rooms,vector<bool>&visited,int i){
-        visited[i]=true;
-        for(auto v:rooms[i]){
-            if(visited[v]==false)
-                dfs(rooms,visited,v);
+    
+    ////DFS
+    
+    
+//     void dfs(vector<vector<int>>& rooms,vector<bool>&visited,int i){
+//         visited[i]=true;
+//         for(auto v:rooms[i]){
+//             if(visited[v]==false)
+//                 dfs(rooms,visited,v);
+//         }
+//     }
+    
+    
+//     bool canVisitAllRooms(vector<vector<int>>& rooms) {
+//         int n=rooms.size();
+//         vector<bool>visited(n,false);
+//         // visited[0]=true;
+//         // for(int i=0;i<n;i++){
+//         //     if(visited[i]==false){
+//         //         dfs(rooms,visited,i,n);
+//         //     }
+//         // }      
+//         dfs(rooms,visited,0);
+        
+        
+//         for(int i=0;i<n;i++){
+//             if(visited[i]==false)
+//                 return false;
+//         }
+//         return true;
+//     }
+    
+    
+////bfs
+    
+        bool canVisitAllRooms(vector<vector<int>>& rooms)
+        {   int n=rooms.size();
+            vector<bool>visited(n,false);
+            queue<int>q;
+            visited[0]=true;
+            q.push(0);
+            while(!q.empty()){
+                int i=q.front();
+                q.pop();
+                for(auto v:rooms[i]){
+                    if(visited[v]==false){
+                        visited[v]=true;
+                        q.push(v);
+                    }
+            }
         }
-    }
-    
-    
-    bool canVisitAllRooms(vector<vector<int>>& rooms) {
-        int n=rooms.size();
-        vector<bool>visited(n,false);
-        // visited[0]=true;
-        // for(int i=0;i<n;i++){
-        //     if(visited[i]==false){
-        //         dfs(rooms,visited,i,n);
-        //     }
-        // }      
-        dfs(rooms,visited,0);
-        
-        
-        for(int i=0;i<n;i++){
+                    for(int i=0;i<n;i++){
             if(visited[i]==false)
                 return false;
         }
         return true;
-    }
+        }
+            
+    
+    
 };
 
 
