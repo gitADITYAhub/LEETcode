54123b989b5592e243dbea8bf199246f0bf4a3a1
gitADITYAhub <99046540+gitADITYAhub@users.noreply.github.com>
2022-07-24 00:55:06 +0530

    Time: 36 ms (42.17%), Space: 12.2 MB (8.69%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -2,15 +2,37 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int>res;
-        for(int i=0;i<nums.size()-1;i++){
-            for(int j=i+1;j<nums.size();j++){
-                if(nums[i]+nums[j]==target){
-                    res.push_back(i);
-                    res.push_back(j);
+//         for(int i=0;i<nums.size()-1;i++){
+//             for(int j=i+1;j<nums.size();j++){
+//                 if(nums[i]+nums[j]==target){
+//                     res.push_back(i);
+//                     res.push_back(j);
 
-                }
+//                 }
+//         }
+//     }
+        
+        unordered_map<int,int>um;
+        for(int i=0;i<nums.size();i++){
+            int temp=target-nums[i];
+            if(um.find(temp)!=um.end()){
+                res.emplace_back(um[temp]);
+
+                res.emplace_back(i);
+            }
+        
+            um[nums[i]]=i;
         }
-    }
+        
       return res;  
     }
 };
+
+        // unordered_map<int, int> indices;
+        // for (int i = 0; i < nums.size(); i++) {
+        //     if (indices.find(target - nums[i]) != indices.end()) {
+        //         return {indices[target - nums[i]], i};
+        //     }
+        //     indices[nums[i]] = i;
+        // }
+        // return {};
