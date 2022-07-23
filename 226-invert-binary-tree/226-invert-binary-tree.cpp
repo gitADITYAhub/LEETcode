9bde1668283f462e3b2fedb60a878db4877b0cb0
gitADITYAhub <99046540+gitADITYAhub@users.noreply.github.com>
2022-07-23 20:58:01 +0530

    Time: 10 ms (6.65%), Space: 9.8 MB (38.60%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.cpp
+++ 226-invert-binary-tree/226-invert-binary-tree.cpp
@@ -11,81 +11,14 @@
  */
 class Solution {
 public:
-    
-//     void invert(TreeNode* root){
-//         if(root->left!=NULL and root->right!=NULL)
-//            swap(root->left,root->right);
-//          if(root->left==NULL){
-//             TreeNode* temp=root->right;
-//                         root->right==NULL;
-
-//             root->left=temp;
-//         }
-//         if(root->right==NULL){
-//             TreeNode* temp=root->left;
-//             root->left==NULL;
-
-//             root->right=temp;
-//         }
-//     }
-    
     TreeNode* invertTree(TreeNode* root) {
-        
-//         if(root==NULL){
-//             return NULL;
-//         }
-//         if(root->left==NULL and root->right==NULL){
-//             return root;
-//         }
-// //         if(root->left==NULL){
-// //             TreeNode* temp=root->right;
-// //                         root->right==NULL;
-
-// //             root->left=temp;
-// //         }
-// //         if(root->right==NULL){
-// //             TreeNode* temp=root->left;
-// //             root->left==NULL;
-
-// //             root->right=temp;
-// //         }
-        
-//         invert(root);
-//         if(root->left)
-//         invert(root->left);
-//         if(root->right)
-//         invert(root->right);
-//         return root;
-        
         if(root==NULL)
             return NULL;
-        
         else{
             invertTree(root->left);
             invertTree(root->right);
             swap(root->left,root->right);
         }
-        
-        
-        
-        
-        
-        
-        
-        return root;
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        return root;    
     }
 };
