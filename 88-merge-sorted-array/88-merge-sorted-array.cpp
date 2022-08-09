12a3e03d26b81d720a9cfcddcb5845440fa5100b
gitADITYAhub <99046540+gitADITYAhub@users.noreply.github.com>
2022-08-09 17:48:00 +0530

    Time: 5 ms (39.84%), Space: 9 MB (89.77%) - LeetHub

---
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i=m-1,j=n-1,k=m+n-1;
+        
+        while(i>=0 and j>=0){
+            if(nums1[i]>nums2[j])
+                nums1[k--]=nums1[i--];
+            else
+                nums1[k--]=nums2[j--];
+            
+        }
+        // while(i<m){
+        //     nums1[k++]=nums1[i++];
+        // }
+        
+        while(j>=0){
+            nums1[k--]=nums2[j--];
+        }
+    }
+};
+
+
+
+
+// int a=m-1;
+//         int b=n-1;
+//         int i=m+n-1;    // calculate the index of the last element of the merged array
+        
+//         // go from the back by A and B and compare and put to the A element which is larger
+//         while(a>=0 && b>=0){
+//             if(A[a]>B[b])   A[i--]=A[a--];
+//             else            A[i--]=B[b--];
+//         }
+        
+//         // if B is longer than A just copy the rest of B to A location, otherwise no need to do anything
+//         while(b>=0)         A[i--]=B[b--];
+    // }
