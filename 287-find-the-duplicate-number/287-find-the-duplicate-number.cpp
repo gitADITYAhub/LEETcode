09dad87e0f17ed086de82dba52d68b7c13675a8d
gitADITYAhub <99046540+gitADITYAhub@users.noreply.github.com>
2022-08-09 16:49:23 +0530

    Time: 208 ms (42.40%), Space: 61.3 MB (27.80%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,26 +1,34 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        unordered_map<int,int>um;
+//         unordered_map<int,int>um;
         int n=nums.size();
         
-        for(int i=0;i<n;i++){
-            if(um.find(nums[i])==um.end()){
-                um.insert({nums[i],1});
-            }
-            else{
-                um[nums[i]]++;
-            }
-        }
-//         for(int i=0;i<um.size();i++){
-//             if(um[i].second>1)
-//                 return um[i].second;
-            
+//         for(int i=0;i<n;i++){
+//             if(um.find(nums[i])==um.end()){
+//                 um.insert({nums[i],1});
+//             }
+//             else{
+//                 um[nums[i]]++;
+//             }
 //         }
-         for (auto& it : um) {
-             if(it.second>1)
-                 return it.first;
-         }
+// //         for(int i=0;i<um.size();i++){
+// //             if(um[i].second>1)
+// //                 return um[i].second;
+            
+// //         }
+//          for (auto& it : um) {
+//              if(it.second>1)
+//                  return it.first;
+//          }
+//         return -1;
+        
+        
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n;i++){
+            if(nums[i]==nums[i+1])
+                return nums[i];
+        }
         return -1;
     }
 };
