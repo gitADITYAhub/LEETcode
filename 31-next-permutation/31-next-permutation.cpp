6adc92d72d8e37905c6df0de239743de3600c38c
gitADITYAhub <99046540+gitADITYAhub@users.noreply.github.com>
2022-08-13 13:31:59 +0530

    Time: 7 ms (58.73%), Space: 12.1 MB (74.63%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,22 +1,42 @@
 class Solution {
 public:
-//     vector<vector<int>>res;
+    // void nextPermutation(vector<int>& nums) {
+    //     // next_permutation(nums.begin(),nums.end());
+    //     int i,j,n=nums.size();
+    //     for( i=n-2;i>=0;i++){
+    //         if(nums[i]<nums[i+1])
+    //             break;
+    //     }
+    //     if(i<0)
+    //         reverse(nums.begin(),nums.end());
+    //     else{
+    //         for(j=n-1;j>i;j--){
+    //             if(nums[j]>nums[i])
+    //                 break;
+    //         }
+    //         swap(nums[j],nums[i]);
+    //         reverse(nums.begin()+j+1,nums.end());
+    //     }
+    // }
     
-//     void helper(vector<int>&nums,int idx){
-//         if(idx==nums.size()){
-//             res.push_back(nums);
-//         }
-//         for(int i=idx;i<nums.size();i++){
-//             swap(nums[idx],nums[i]);
-//             helper(nums,idx+1);
-//             swap(nums[idx],nums[i]);
-//         }
-        
-//     }
-    
-    
-    void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
+        void nextPermutation(vector<int>& nums) {
+        int n = nums.size(), k, l;
+    	for (k = n - 2; k >= 0; k--) {
+            if (nums[k] < nums[k + 1]) {
+                break;
+            }
+        }
+    	if (k < 0) {
+    	    reverse(nums.begin(), nums.end());
+    	} else {
+    	    for (l = n - 1; l > k; l--) {
+                if (nums[l] > nums[k]) {
+                    break;
+                }
+            } 
+    	    swap(nums[k], nums[l]);
+    	    reverse(nums.begin() + k + 1, nums.end());
+        }
     }
     
 };
