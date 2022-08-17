284c3b3fd8a159d885470e0cc197f9484facfc2f
gitADITYAhub <99046540+gitADITYAhub@users.noreply.github.com>
2022-08-17 17:07:39 +0530

    Time: 8 ms (80.99%), Space: 14.9 MB (13.87%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -11,19 +11,40 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(list1==NULL)
-            return list2;
-        if(list2==NULL)
-            return list1;
-        if(list1->val<list2->val){
-            list1->next=mergeTwoLists(list1->next,list2);
-            return list1;
-        }
-        else{
-            list2->next=mergeTwoLists(list1,list2->next);
-            return list2;
+        // if(list1==NULL)
+//             return list2;
+//         if(list2==NULL)
+//             return list1;
+//         if(list1->val<list2->val){
+//             list1->next=mergeTwoLists(list1->next,list2);
+//             return list1;
+//         }
+//         else{
+//             list2->next=mergeTwoLists(list1,list2->next);
+//             return list2;
 
+//         }
+        ListNode *head=new ListNode(0);
+        ListNode *cur=head;
+        
+        while(list1 and list2){
+            if(list1->val<=list2->val){
+                cur->next=list1;
+                list1=list1->next;
+            }
+            else{
+                cur->next=list2;
+                list2=list2->next;
+            }
+            
+            cur=cur->next;
         }
+        if(list1)
+            cur->next=list1;
+        if(list2)
+            cur->next=list2;
+        
+        return head->next;
         
     }
 };
