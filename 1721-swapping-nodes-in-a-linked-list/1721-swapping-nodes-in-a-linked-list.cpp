08d4a73eb9be8aebe3995c99e99a5880b52a4946
gitADITYAhub <99046540+gitADITYAhub@users.noreply.github.com>
2022-08-17 21:41:18 +0530

    Time: 1661 ms (5.08%), Space: 179.9 MB (99.10%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
@@ -11,21 +11,35 @@
 class Solution {
 public:
     ListNode* swapNodes(ListNode* head, int k) {
-        int c1=0;
-        ListNode* cur=head;
-        while(cur){
-            c1++;
-            cur=cur->next;
-        }
-        int c2=c1-k+1;
-        ListNode* prev=head;
-        for(int i=1;i<k;i++)
-            prev=prev->next;
-        cur=head;
-        for(int i=1;i<c2;i++)
-            cur=cur->next;
+//         int c1=0;
+//         ListNode* cur=head;
+//         while(cur){
+//             c1++;
+//             cur=cur->next;
+//         }
+//         int c2=c1-k+1;
+//         ListNode* prev=head;
+//         for(int i=1;i<k;i++)
+//             prev=prev->next;
+//         cur=head;
+//         for(int i=1;i<c2;i++)
+//             cur=cur->next;
         
-        swap(prev->val,cur->val);
+//         swap(prev->val,cur->val);
+        
+//         return head;
+        
+        ListNode*slow=head,*fast=head;
+        
+        while(--k)
+            fast=fast->next;
+        ListNode*temp=fast;
+        while(fast->next){
+            slow=slow->next;
+            fast=fast->next;
+        }
+        
+        swap(slow->val,temp->val);
         
         return head;
     }
