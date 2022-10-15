d5044822158d61967ce54537c1c223b816bc1daf
gitADITYAhub <99046540+gitADITYAhub@users.noreply.github.com>
2022-10-15 17:39:55 +0530

    Time: 5 ms (32.02%), Space: 8.3 MB (23.26%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -2,13 +2,45 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int n=nums.size();
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[i]>nums[j]){
-                    swap(nums[i],nums[j]);
-                }
+//         for(int i=0;i<n;i++){
+//             for(int j=i+1;j<n;j++){
+//                 if(nums[i]>nums[j]){
+//                     swap(nums[i],nums[j]);
+//                 }
                     
-            }
+//             }
+//         }
+        int a=0,b=0,c=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==0)
+               a++;
+            else if(nums[i]==1)
+                b++;
+            else if(nums[i]==2)
+                c++;
         }
+        for(int i=0;i<a;i++)
+            nums[i]=0;
+        for(int i=0;i<b;i++)
+            nums[a+i]=1;
+        for(int i=0;i<c;i++)
+            nums[a+b+i]=2;
     }
 };
+// void sortColors(int A[], int n) {
+//     int num0 = 0, num1 = 0, num2 = 0;
+    
+//     for(int i = 0; i < n; i++) {
+//         if (A[i] == 0) ++num0;
+//         else if (A[i] == 1) ++num1;
+//         else if (A[i] == 2) ++num2;
+//     }
+    
+//     for(int i = 0; i < num0; ++i) A[i] = 0;
+//     for(int i = 0; i < num1; ++i) A[num0+i] = 1;
+//     for(int i = 0; i < num2; ++i) A[num0+num1+i] = 2;
+// }
+
+// for(int i = 0; i < num0; ++i) A[i] = 0;
+//     for(int i = 0; i < num1; ++i) A[num0+i] = 1;
+//     for(int i = 0; i < num2; ++i) A[num0+num1+i] = 2;
