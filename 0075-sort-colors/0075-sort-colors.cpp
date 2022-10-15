8e2286348a7e14bc8b23093e29fcb229bd9cf320
gitADITYAhub <99046540+gitADITYAhub@users.noreply.github.com>
2022-10-15 17:49:46 +0530

    Time: 7 ms (18.39%), Space: 8.2 MB (90.71%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -10,20 +10,39 @@ public:
                     
 //             }
 //         }
-        int a=0,b=0,c=0;
-        for(int i=0;i<n;i++){
-            if(nums[i]==0)
-               a++;
-            else if(nums[i]==1)
-                b++;
-            else if(nums[i]==2)
-                c++;
+        // int a=0,b=0,c=0;
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]==0)
+        //        a++;
+        //     else if(nums[i]==1)
+        //         b++;
+        //     else if(nums[i]==2)
+        //         c++;
+        // }
+        // for(int i=0;i<a;i++)
+        //     nums[i]=0;
+        // for(int i=0;i<b;i++)
+        //     nums[a+i]=1;
+        // for(int i=0;i<c;i++)
+        //     nums[a+b+i]=2;
+        int low=0,mid=0,high=n-1;
+        while(mid<=high){
+            switch(nums[mid]){
+                case 0:
+                swap(nums[low],nums[mid]);
+                low++;
+                mid++;
+                break;
+            
+                case 1:
+                mid++;
+                break;
+                case 2:
+                swap(nums[mid],nums[high]);
+                high--;
+                break;
+            }
+            
         }
-        for(int i=0;i<a;i++)
-            nums[i]=0;
-        for(int i=0;i<b;i++)
-            nums[a+i]=1;
-        for(int i=0;i<c;i++)
-            nums[a+b+i]=2;
     }
 };
