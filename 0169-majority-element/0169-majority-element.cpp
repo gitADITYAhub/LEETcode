08760d2270850ab9929a5fe10b07f3f966aef235
gitADITYAhub <99046540+gitADITYAhub@users.noreply.github.com>
2022-11-06 22:59:41 +0530

    Time: 22 ms (89.47%), Space: 19.6 MB (60.38%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,14 +1,27 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int,int>um;
+        // unordered_map<int,int>um;
+        // for(int i=0;i<nums.size();i++){
+        //     um[nums[i]]++;
+        // }
+        // for(int i=0;i<um.size();i++){
+        //     if(um[i]>nums.size()/2)
+        //         return i;
+        // }
+        // return 0;
+        int elem=0,c=0;
         for(int i=0;i<nums.size();i++){
-            um[nums[i]]++;
+            if(c==0){
+                elem=nums[i];
+            }
+            if(elem==nums[i]){
+                c++;
+            }
+            else{
+                c--;
+            }
         }
-        for(int i=0;i<um.size();i++){
-            if(um[i]>nums.size()/2)
-                return i;
-        }
-        return 0;
+        return elem;
     }
 };
