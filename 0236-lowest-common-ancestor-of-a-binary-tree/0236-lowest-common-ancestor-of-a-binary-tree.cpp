060afb683cf34c83a1b2433468b2a70dd2538a82
gitADITYAhub <99046540+gitADITYAhub@users.noreply.github.com>
2022-11-09 16:59:11 +0530

    Time: 45 ms (12.56%), Space: 14.2 MB (56.56%) - LeetHub

---
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    
+    bool papath(TreeNode* root,vector<TreeNode*> &path,TreeNode* x){
+        if(!root)
+            return false;
+        path.push_back(root);
+        if(root==x){
+            return true;
+        }
+        
+        if( papath(root->left,path,x) || papath(root->right,path,x)){
+            return true;
+        }
+        
+        path.pop_back();
+        return false;
+    
+    }
+    
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        // vector<TreeNode*>ppath,qpath;
+        // if(!papath(root,ppath,p) || !papath(root,qpath,q))
+        //     return NULL;
+        // TreeNode*ans=NULL;
+        // for(int i=0; i<ppath.size() && i<qpath.size(); i++){
+        //     if(ppath[i] == qpath[i])
+        //         ans = ppath[i];
+        // }
+        // return ans;
+        if(root==NULL or root==p or root==q) return root;
+        TreeNode*left=lowestCommonAncestor(root->left,p,q);
+        TreeNode* right=lowestCommonAncestor(root->right,p,q);
+        if(left==NULL) return right;
+        if(right==NULL) return left;
+        else return root;
+    }
+};
