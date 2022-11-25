7fbfd7c50160fda127aa5710c1d7b7c1a3769d84
gitADITYAhub <99046540+gitADITYAhub@users.noreply.github.com>
2022-11-25 22:32:45 +0530

    Time: 11 ms (84.92%), Space: 7.3 MB (56.05%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,8 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return (s==t);
+        // sort(s.begin(),s.end());
+        // sort(t.begin(),t.end());
+        // return (s==t);
+        if(s.size()>t.size())return false;
+        unordered_map <char, int> umap;
+        for (char c:s) umap[c]++;
+        for(char c:t) {
+            umap[c]--;
+            if (umap[c]<0) return false;
+        }
+        
+        return true;
     }
 };
