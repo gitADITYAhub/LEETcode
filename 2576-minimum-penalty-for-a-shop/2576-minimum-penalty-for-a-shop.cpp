2f8e5a59784a9ae30cdf93e84ebc8ffb4ec4c29f
gitADITYAhub <99046540+gitADITYAhub@users.noreply.github.com>
2023-08-29 23:23:53 +0530

    Time: 37 ms (39.38%), Space: 18.9 MB (26.64%) - LeetHub

--- 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.cpp
+++ 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.cpp
@@ -1,24 +1,56 @@
 class Solution {
 public:
     int bestClosingTime(string customers) {
-        int n = customers.size();
-        int maximum = 0;
-        int idx= 0;
-        pair<int,int> count = {0,0};
-        for(int i = 0;i<n;i++){
-            if(customers[i] == 'Y'){
-                count.first++;
-                count.second = i+1;
-            }else {
-                count.first--;
-                count.second = i;
-            }
-            if(count.first>maximum){
-                idx = count.second;
-            }
-            maximum = max(maximum,count.first);
+        // int n = customers.size();
+        // int maximum = 0;
+        // int idx= 0;
+        // pair<int,int> count = {0,0};
+        // for(int i = 0;i<n;i++){
+        //     if(customers[i] == 'Y'){
+        //         count.first++;
+        //         count.second = i+1;
+        //     }else {
+        //         count.first--;
+        //         count.second = i;
+        //     }
+        //     if(count.first>maximum){
+        //         idx = count.second;
+        //     }
+        //     maximum = max(maximum,count.first);
             
+        // }
+        // return idx;
+
+
+
+        int n = customers.size();
+        vector<int> right(n+1,0);
+        vector<int> left(n+1,0);
+        left[0] = 0;
+
+        for(int i = 1;i<n+1;i++){
+            if(customers[i-1] == 'N'){
+                left[i] = left[i-1] +1;
+            }
+            else left[i] = left[i-1];
         }
-        return idx;
+
+        right[n] = 0;
+        for(int i = n-1;i>=0;i--){
+            if(customers[i] == 'Y'){
+                right[i] = right[i+1] +1;
+            }
+            else right[i] = right[i+1];
+        }
+        
+        int index = 0;
+        int min1 = 1e9;
+        for(int i  = 0;i<n+1;i++){
+            if (right[i]+left[i] < min1){
+                min1 = right[i]+left[i];
+                index = i;
+            }
+        }
+        return index;
     }
 };
