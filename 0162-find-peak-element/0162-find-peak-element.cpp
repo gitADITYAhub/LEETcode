d4ec2a935982ce6814193f2c439dc2922415744a
gitADITYAhub <99046540+gitADITYAhub@users.noreply.github.com>
2023-09-14 11:48:05 +0530

    Time: 0 ms (100%), Space: 9.1 MB (19.99%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,39 +1,18 @@
 class Solution {
 public:
-    int findPeakUtil(vector<int>&arr,int low,int high,int n){
-        // int mid=i+(j-i)/2;
-        // if(mid==0||nums[mid-1]<=nums[mid] && mid==n-1||nums[mid+1]<=nums[mid])
-        //     return mid;
+    int helper(vector<int>&nums,int i,int j,int n){
+        int mid=i+(j-i)/2;
+        if((mid==0||nums[mid-1]<=nums[mid]) && (mid==n-1||nums[mid+1]<=nums[mid]))
+            return mid;
         
-        // else if(mid>0 && nums[mid-1]>nums[mid]){
-        //     return helper(nums,i,mid-1,n);
-        // }
-        // else{
-        //     return helper(nums,mid+1,j,n);
-        // }
-        int mid = low + (high - low) / 2;
- 
-    // Compare middle element with its
-    // neighbours (if neighbours exist)
-    if ((mid == 0 || arr[mid - 1] <= arr[mid]) &&
-        (mid == n - 1 || arr[mid + 1] <= arr[mid]))
-        return mid;
- 
-    // If middle element is not peak and its
-    // left neighbour is greater than it,
-    // then left half must have a peak element
-    else if (mid > 0 && arr[mid - 1] > arr[mid])
-        return findPeakUtil(arr, low, (mid - 1), n);
- 
-    // If middle element is not peak and its
-    // right neighbour is greater than it,
-    // then right half must have a peak element
-    else
-        return findPeakUtil(
-            arr, (mid + 1), high, n);
+        else if(mid>0 && nums[mid-1]>nums[mid]){
+            return helper(nums,i,mid-1,n);
+        }
+        else{
+            return helper(nums,mid+1,j,n);
+        }
     }
     int findPeakElement(vector<int>& nums) {
-        int n=nums.size();
-        return findPeakUtil(nums, 0, n - 1, n);
+        return helper(nums,0,nums.size()-1,nums.size());
     }
 };
